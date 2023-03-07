f9849eb00caa15912ca3efb6db179edeb4f0a408
TheoRafuse <124588617+TheoRafuse@users.noreply.github.com>
2023-03-07 15:00:55 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 Theo Rafuse All rights reserved
+// Created by: Theo Rafuse
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello World"
+
+  document.getElementById("hello-world").innerHTML = "<p>Because they make up everything!</p>"
+}
